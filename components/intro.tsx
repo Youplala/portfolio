f085880b704d5e95ae42ba6cd095d6eb7ec031b1
Elie Brosset <32967983+Youplala@users.noreply.github.com>
2023-12-06 10:16:42 +0100

    Update intro.tsx

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,7 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
-import profileImg from "@/public/profile_pic.png";
+import profileImg from "@/public/profile_pic.jpg";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
